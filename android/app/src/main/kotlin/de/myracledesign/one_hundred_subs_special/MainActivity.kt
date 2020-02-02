70264926e3e45604d553cf94761c59f82476e19b
myracledesign <weber.max.1988@live.de>
2020-02-02 22:13:57 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/de/myracledesign/one_hundred_subs_special/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.myracledesign.one_hundred_subs_special
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
